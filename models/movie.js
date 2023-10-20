a6f27b6ff22c7f88e19bb000683feb1b57bdecd0
Abay <ibrayev.abay@gmail.com>
2023-10-20 10:12:56 -0600

    data validation

--- models/movie.js
+++ models/movie.js
@@ -3,9 +3,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const movieSchema = new Schema({
-    title: String,
-    releaseYear: Number,
-    mpaaRating: String,
+    title: { type: String, required: true },
+    releaseYear: {
+        type: Number,
+        default: function() {
+            return new Date().getFullYear();
+        },
+        min: 1927
+    },
+    mpaaRating: {
+        type: String,
+        enum: ['G', 'PG', 'PG-13', 'R']
+    },
     cast: [String],
     nowShowing: Boolean
 }, {
